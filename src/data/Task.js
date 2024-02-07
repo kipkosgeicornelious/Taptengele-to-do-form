812efaba08765a0c311b7e56e649c793faf46b27
kipkosgeicornelious <kipkosgeicornelious6@gmail.com>
2024-02-07 14:47:06 +0300

    done

---
+++ src/data/Task.js
@@ -0,0 +1,8 @@
+const todos = [
+    { id: 1, title: 'Reading novel'},
+    { id: 2, title: 'Play a game'},
+    { id: 3, title: 'Practice code challenge'},
+    { id: 4, title: 'Hiking'},
+    { id: 5, title: 'Go for dinner'},   
+]
+export default todos;
